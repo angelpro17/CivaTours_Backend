b60beb02bef26d53cdd34c5c26dd8135ffa8233e
Luis Ángel Anampa Lavado <u202218664@upc.edu.pe>
2025-10-02 17:16:52 -0500

    feat: add structure initial

---
+++ src/main/java/com/civa/challenge/civatours/CivaToursApplication.java
@@ -0,0 +1,13 @@
+package com.civa.challenge.civatours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivaToursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivaToursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/civa/challenge/civatours/CivaToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civa.challenge.civatours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivaToursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
